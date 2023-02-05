30dd839ab467ac016a43190330a5992c4a620b82
nmorse <morse.nate@gmail.com>
2023-02-05 14:39:59 -0500

    about nothing

---
+++ src/routes/about.tsx
@@ -0,0 +1,17 @@
+import { Title } from "solid-start";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main>
+      <Title>About</Title>
+      <h1>a techable blog</h1>
+      <p>
+Teachable Technology is the over ambitious goal. 
+But really what you'll find is just a blog that shares some positive headway made in areas of interest. expect website and microprocessor, hardware and software projects. 
+There are more ideas than time allows to solidify into projects and more projects than time allows to be written up into blog posts, so here are the easiest projects, simplistically described. Enjoy!
+nmorse.github.com @natemorse
+      </p>
+    </main>
+  );
+}
