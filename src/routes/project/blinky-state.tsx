de5f1736f560aafea54f56160ac1b014099ce6f3
nmorse <morse.nate@gmail.com>
2023-02-18 16:34:47 -0500

    separate two

--- src/routes/project/blinky-state.tsx
+++ src/routes/project/blinky-state.tsx
@@ -4,66 +4,47 @@ export default function Home() {
   return (
     <main>
       <Title>Blinky State</Title>
-      <h1>A Reunion of New and Old Tech</h1>
-      <h2>Mixing new micro-controllers with old software techniques</h2>
+      <h1>Blinky done with Two States</h1>
+      <h2>The "Hello World!" of Micro-controllers coded with a Statemachine</h2>
       <p>
-        The microcontoller and 'makers' have had such a successful run,
-        with breakout boards that keep beginners from making bad mistatkes
-        to code libraies that tame complex hardware into accessable,
-        effective electronics.
-        Eight bit processors have been all but replaced by super-capable 32 bit,
-        multi core systems at the same price and size. A few meta-makers
-        of these breakout boards are keeping up with the times in setting up
-        easy to use libraries in accessable languages.
+        This project is a simple one that demonstraits a slightly complicated way to acheve a simple blinking LED.
+        Why complicate this? It sets up some really nice methods for managing more complex projects in a simpler way.
+        Here is the thing, there are simpler blinky toutorials, but the methods they use will not scale, instead here we set you up for success in your next project.
       </p>
       <p>
-        <hr />
-        Two great suppliers (meta-makers) are:
+        So, here let's introduce (in a simple way) two advanced methods used to controle complexity in a larger project:
         <ul>
-          <li><a href="https://adafruit.com/" target="new">Adafruit</a> hardware with their well supported Circuitpython software
-            have mapped out a path for maker success!
-            Smoothing out the leaning curve keeps makers successful and
-            teaches annalog and digital electronics allong the way.
+          <li> cooperative multi-tasking in the form of circuitpython's asyncio</li>
+          <li> and the use of a statemachine, using xState and their new visual environment, stately.ai</li>
+        </ul>
+        "Why" use these two old techniques in a modern microcontroller is the subject of a separate artcle <A href="./asyncio-and-statemachine">the merrits of asyncio and statemachines</A>,
+        but here we moving forward, to get to the "blinking."  
+        </p>
+      <p>
+        <hr />
+        Here are the hardware ingredients:
+        <ul>
+          <li>a (newer) micro-controller <a href="https://circuitpython.org/downloads?q=asyncio" target="new">board that can run asyncio </a> (one that has some kind of LED for blinking)
           </li>
-          <li>and <a href="https://sparkfun.com/" target="new">Sparkfun</a> also makes high quality boards.
+          <li>and a USB cord to connect it to your computer
           </li>
         </ul>
-        Lots of others good providers, but these IMHO are Great!
         <hr />
       </p>
       <p>
-        All this capability is the new magic carpet, but before we go for a
-        ride, how can we tell our microcontroller what we want it to do?
-        The common way, especially for beginners is to write step by step
-        instructions. For simple projects this is straight forward, so you might
-        think that is the best approach to give someone, just starting out, a short
-        path to success.
-        Could there be any down-side? Well, yes, as beginners find success and want to move
-        on to intermediate projects, the step by step instruction method quickly becomes
-        untenable. Computer Scientists call this style of instruction 'Procedural' programming.
+        Next we need to load some software: The nice thing is that we can do this all via a USB cable.
+        First we need circuitpython version 8 or greater. and some additional libraries 
+        to help asyncio work.
+
+        then load the project from this git repo ... and it should work for these boards:
+
+
+        
+        (other boards from the link above "board[s] that can run asyncio" should also work with a few minor changes)
       </p>
       <p>
-
-        We want to try out a differnet approach. 'Declaritive' programming takes a
-        different tact, where we tell the microcontroller "what" we want it to do and less
-        of "how exactly" to do it.
-        Ok, that is maybe too abstract, but put another way, if we tell it how to respond to situations
-        we avoid many pitfalls of telling it every exacting step one after the next.
-        <hr />
-        Declarative contrasts with Procedural programming (which merely acts, rather than reacts)
-        <hr />
-        So we will be declaring the behavior we want, and to do this, we'll use a wonderfull
-        thing called a 'State Machine.' We'll define the "States" that are usefull and what "Events"
-        should cause it to change states. There is so much written about the use of state machines
-        in programming* and it might feel as if we are jumping in to the deep end of the pool,
-        but our use will be as basic as it gets and 'State Machines' are naturally intuitive. Also
-        we will be using a visual representation that can help you see how they work.
       </p>
       <p>
-
-        The project is the "hello world!" of microcontrollers, the blinking LED.
-        but wait, 
-
       </p>
     </main>
   );
